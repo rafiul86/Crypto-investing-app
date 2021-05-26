c1f6e5e2178dfb70a9a049af61eba98f98a2dc04
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-26 16:03:44 +0600

    feature  added

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Featured from './featured/Featured';
 import HeaderMain from './headerMain/HeaderMain';
 
 const Header = () => {
     return (
         <div>
             <HeaderMain />
+            <Featured />
         </div>
     );
 };

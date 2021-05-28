770a70b80453977231593b8b6f54558b1d3f7df7
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-28 17:25:06 +0600

    activity

--- src/components/learn/tutorial/Tutorial.js
+++ src/components/learn/tutorial/Tutorial.js
@@ -2,13 +2,15 @@ import React from 'react';
 import SideBar from '../sideBar/SideBar';
 import NavBar from '../../shared/navBar/NavBar'
 const Tutorial = ({progressDetails}) => {
+    console.log(progressDetails)
     return (
         <div className="activity-design">
             <div>
             <button className="btn-course-progress">completed</button>
             </div>
             <h3 className="text-white">{progressDetails.header}{' '} : {progressDetails.title}</h3>
-            <p className="text-white">{progressDetails.completePercentage}</p>
+            <p className="text-white">{progressDetails.activity}</p>
+            <p className="text-white">{progressDetails.activity}</p>
         </div>
     );
 };

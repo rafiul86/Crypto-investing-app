318f60a13ed6f7aa334d8a1bfb6538a85d776481
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-28 19:35:25 +0600

    video link added

--- src/components/learn/progress/ProgressList.js
+++ src/components/learn/progress/ProgressList.js
@@ -6,11 +6,14 @@ const ProgressList = ({list}) => {
         
     return (
         <div >
-        <div className="activity-design">
-            <div>
+        <div className="activity-design row">
+        <div className="col-sm-3">
+        <iframe width="130" height="85" src="https://www.youtube.com/embed/4Wp43whakbU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+            <div className="col-sm-9">
             <button className="btn-course-progress">completed</button>
             </div>
-            <h3 className="text-white"><Link to={`/progressAnalysis/${list.id}`}>{list.period}{' '} : {list.title}</Link></h3>
+            <h5 className="text-white"><Link className="tutorial-link" to={`/progressAnalysis/${list.id}`}>{list.period}{' '} : {list.title}</Link></h5>
             <img className="image-progress" src={progressbar}/>
             <p className="text-white">{list.completePercentage}</p>
         </div>
--- src/components/learn/progress/progressHeader/ProgressHeader.js
+++ src/components/learn/progress/progressHeader/ProgressHeader.js
@@ -5,11 +5,15 @@ import '../progress.css'
 const ProgressHeader = ({featuredCourseDetails}) => {
     return (
         <div className="progress-header-container">
-           <h1>{featuredCourseDetails.period}</h1>
+          <div className="progress-head-period">
+          <h1>{featuredCourseDetails.period}</h1>
+          </div>
+           <div className="progress-head-period">
            <h4>Progress</h4>
-           <h4>{featuredCourseDetails.module}</h4>
+           </div>
+           <h4 className="text-white">{featuredCourseDetails.module}</h4>
            <img className="image-progress" src={progressbar}/>
-           <h4>{featuredCourseDetails.completePercentage}</h4>
+           <h4 className="text-white">{featuredCourseDetails.completePercentage}</h4>
         </div>
     );
 };

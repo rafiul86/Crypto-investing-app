880af75f4f7ba16bdc0939f542afdbfc66d1ec58
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-28 17:08:47 +0600

    crypto added

--- src/components/learn/welcome/welComeHeader/WelComeHeader.js
+++ src/components/learn/welcome/welComeHeader/WelComeHeader.js
@@ -12,7 +12,7 @@ const WelComeHeader = ({handleClick}) => {
                 </Grid>
                 <Grid item xs={10} md={5} lg={6}>
                     <div style={{textAlign : 'left'}}>
-                    <h1 className="welcome-headline text-white">Welcome back, <br />
+                    <h1 className="welcome-headline mt-5 text-white">Welcome back, <br />
                     Shot</h1>
                     <h3 style={{fontStyle : 'italic', alignItems: 'center', color: "white"}}> 75% <span style={{fontStyle : 'italic' , color: "white"}}>Completed</span></h3>
                     <button className="btn-resume" onClick={handleClick}>Resume</button>
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,36 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router';
+import crypto from '../../images/crypto.jpg';
 import NavBar from '../shared/navBar/NavBar';
 
+
 const Main = () => {
+    const history = useHistory()
+
+    const handleClick = () =>{
+        history.push('/learn')
+    }
     return (
-        <div>
+        <div className="mb-5">
             <NavBar />
-            <h1>Minority Programmers Association</h1>
-            <h3>Code</h3>
-            <h3>Culture</h3>
-            <h3>Community</h3>
+            <Grid container>
+                <Grid item xs={10} md={5} lg={6}>
+                <img className="img-holder" src={crypto} />
+                </Grid>
+                <Grid item xs={10} md={5} lg={6}>
+                    <div style={{textAlign : 'left'}}>
+                    <h1 className="welcome-headline text-white">Minority programmers association <br />
+                    Code, Culture, Community</h1>
+                    <h3 style={{fontStyle : 'italic', alignItems: 'center', color: "white"}}>Sanctuary of  <span style={{fontStyle : 'italic' , color: "#151371"}}>Legends</span></h3>
+                    <button className="btn-resume" onClick={handleClick}>Start journey</button>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     );
 };
+
+
 
 export default Main;

22577bbce6fcac74814028978cebdb121084d78c
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-29 15:29:19 +0600

    line stroke

--- src/components/learn/featuredCourses/FeaturedCourses.js
+++ src/components/learn/featuredCourses/FeaturedCourses.js
@@ -10,7 +10,14 @@ const FeaturedCourses = () => {
   }, []);
   return (
     <div>
-      <h1 style={{ textAlign: "start", color: "white", padding : 30 }}>Featured Courses</h1>
+      <div className="row">
+        <div className="col-sm-4">
+        <h1 style={{ textAlign: "start", color: "white", padding : 30 }}>Featured Courses</h1>
+        </div>
+        <div className="col-sm-8">
+        <div style={{ borderTop: "3px solid #fff ", margin: 60 }}></div>
+        </div>
+      </div>
       <Grid container spacing={4}>
         {featuredCourses.map((featuredCourse) => (
           <Grid item xs={12} md={6} lg={4} key={featuredCourse.id}>
--- src/components/learn/myCourses/MyCourses.js
+++ src/components/learn/myCourses/MyCourses.js
@@ -2,6 +2,9 @@ import { Grid } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import MyCourse from "./myCourse/MyCourse";
 import myCoursesData from "./myCourses.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 const MyCourses = () => {
   const [myCourses, setMyCourses] = useState([]);
   useEffect(() => {
@@ -9,7 +12,14 @@ const MyCourses = () => {
   }, []);
   return (
     <div>
-      <h1 style={{ textAlign: "start", color: "white", padding : 30 }}>My Courses</h1>
+      <div className="row">
+        <div className="col-sm-4">
+        <h1 style={{ textAlign: "start", color: "white", padding : 30 }}>Featured Courses</h1>
+        </div>
+        <div className="col-sm-8">
+        <div style={{ borderTop: "3px solid #fff ", margin: 60 }}></div>
+        </div>
+      </div>
       <Grid container spacing={4}>
         {myCourses.map((myCourse) => (
           <Grid item xs={12} md={6} lg={4} key={myCourse.id}>

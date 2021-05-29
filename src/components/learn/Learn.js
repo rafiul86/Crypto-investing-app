38056943dba3762cb033c75f87620a5baad59f66
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-05-29 15:50:57 +0600

    search option included

--- src/components/learn/Learn.js
+++ src/components/learn/Learn.js
@@ -5,11 +5,24 @@ import LearnHeader from "./learnHeader/LearnHeader";
 import MyCourses from "./myCourses/MyCourses";
 import "./bottomLearn.css";
 const Learn = () => {
+  const handleBlur = () =>{
+      console.log('desired course from database')
+  }
   return (
     <div>
       <NavBar />
       <LearnHeader />
       <div className="bottom-body">
+      <form style={{padding : 60, alignSelf : 'center'}}>
+  <div className="row">
+    <div className="col-sm-8">
+    <input type="name" className="form-control" onBlur={handleBlur} name="name" />
+    </div>
+    <div className="col-md-2">
+    <button type="submit" className="btn btn-light">Filter Search</button>
+    </div>
+  </div>
+</form> 
         <MyCourses />
         <FeaturedCourses />
       </div>
